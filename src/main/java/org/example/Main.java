d062200003897b78cec8e8c26abf9619913c3e5d
osamaline1229 <osama98138139@gmail.com>
2023-12-20 10:59:14 +0400

    filter the unique elements present in an array of strings.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,24 @@
 package org.example;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 
- class NumberFrequency {
+class UniqueElementsFilter {
     public static void main(String[] args) {
-        int[] inputArray = {2, 1, 3, 2, 1, 7, 3, 6, 1,7, 2, 58,58, 66, 7};
-        printNumberFrequency(inputArray);
+        String[] inputArray = {"A", "B", "C", "A", "D", "B", "Z", "Y", "R", "Z"};
+        String[] uniqueElements = filterUniqueElements(inputArray);
+
+        // Print the unique elements
+        System.out.println(Arrays.toString(uniqueElements));
     }
 
-    private static void printNumberFrequency(int[] inputArray) {
-        // Create a HashMap to store the frequency of each number
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
+    private static String[] filterUniqueElements(String[] inputArray) {
+        // Create a HashSet to store unique elements
+        Set<String> uniqueSet = new HashSet<>(Arrays.asList(inputArray));
 
-        // Count the frequency of each number in the array
-        for (int num : inputArray) {
-            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
-        }
-
-        // Print the frequency of each number
-        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-            int number = entry.getKey();
-            int frequency = entry.getValue();
-            System.out.println(number + " is repeated " + frequency + " times");
-        }
+        // Convert the HashSet back to an array
+        return uniqueSet.toArray(new String[0]);
     }
 }
