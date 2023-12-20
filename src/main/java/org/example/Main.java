0138889919ff991e28b269ae319b7b33f957dbb1
osamaline1229 <osama98138139@gmail.com>
2023-12-20 10:55:39 +0400

    get duplicated times

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,35 +1,29 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 
-
-class ArrayCompression {
+ class NumberFrequency {
     public static void main(String[] args) {
-        int[] inputArray = {4, 3, 1, 1, 3, 5, 5, 8, 8, 8, 9,10,10,10};
-        int[] compressedArray = compressArray(inputArray);
-
-        // Print the compressed array
-        for (int element : compressedArray) {
-            System.out.print(element + " ");
-        }
+        int[] inputArray = {2, 1, 3, 2, 1, 7, 3, 6, 1,7, 2, 58,58, 66, 7};
+        printNumberFrequency(inputArray);
     }
 
-    private static int[] compressArray(int[] inputArray) {
-        Set<Integer> uniqueElements = new HashSet<>();
-        List<Integer> compressedList = new ArrayList<>();
+    private static void printNumberFrequency(int[] inputArray) {
+        // Create a HashMap to store the frequency of each number
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
 
-        for (int element : inputArray) {
-            // Add the element to the compressed list if it's not already present
-            if (uniqueElements.add(element)) {
-                compressedList.add(element);
-            }
+        // Count the frequency of each number in the array
+        for (int num : inputArray) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
         }
 
-        // Convert the List to an array
-        return compressedList.stream().mapToInt(Integer::intValue).toArray();
+        // Print the frequency of each number
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            int number = entry.getKey();
+            int frequency = entry.getValue();
+            System.out.println(number + " is repeated " + frequency + " times");
+        }
     }
 }
